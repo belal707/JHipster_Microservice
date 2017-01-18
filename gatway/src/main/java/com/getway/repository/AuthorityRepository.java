8a48e9176d668d94e3dc3a6fd772a1252157d680
Belal Ansari <belalansari23@gmail.com>
2017-01-18 16:43:59 +0530

    initial comit for getway

---
+++ gatway/src/main/java/com/getway/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.getway.repository;
+
+import com.getway.domain.Authority;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Authority entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {
+}
---
+++ gatway/src/main/java/com/getway/web/filter/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Servlet filters.
+ */
+package com.getway.web.filter;
